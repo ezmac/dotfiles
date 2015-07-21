cff7e1cb2a7abebb0db5e80722f19cade7838926
Tad Merchant <system.root@gmail.com>
2015-07-21 17:42:43 -0400

    touch up linker

--- linkDotfiles.sh
+++ linkDotfiles.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-ln -s /home/tad/.dotfiles/.vimrc /home/tad/.vimrc
-ln -s /home/tad/.dotfiles/.zshrc /home/tad/.zshrc
-ln -s /home/tad/.dotfiles/ezmac.zsh-theme /home/tad/.oh-my-zsh/themes/
-ln -s /home/tad/.dotfiles/.tmux.conf /home/tad/.tmux.conf
-ln -s /home/tad/.dotfiles/.inputrc /home/tad/.inputrc
-ln -s /home/tad/.dotfiles/prompt_randomizer.js /home/tad/.prompt_randomizer.js
+ln -s `pwd`/.vimrc ~/.vimrc
+ln -s `pwd`/.zshrc ~/.zshrc
+ln -s `pwd`/ezmac.zsh-theme ~/.oh-my-zsh/themes/
+ln -s `pwd`/.tmux.conf ~/.tmux.conf
+ln -s `pwd`/.inputrc ~/.inputrc
+ln -s `pwd`/prompt_randomizer.js ~/.prompt_randomizer.js
 
 
