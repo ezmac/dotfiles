e760e3570fbe12ec01e960c134162c10a387f67f
Tad Merchant <system.root@gmail.com>
2015-07-21 17:35:27 -0400

    dot file linker

---
+++ linkDotfiles.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ln -s /home/tad/.dotfiles/.vimrc /home/tad/.vimrc
+ln -s /home/tad/.dotfiles/.zshrc /home/tad/.zshrc
+ln -s /home/tad/.dotfiles/ezmac.zsh-theme /home/tad/.oh-my-zsh/themes/
+ln -s /home/tad/.dotfiles/.tmux.conf /home/tad/.tmux.conf
+ln -s /home/tad/.dotfiles/.inputrc /home/tad/.inputrc
+ln -s /home/tad/.dotfiles/prompt_randomizer.js /home/tad/.prompt_randomizer.js
+
+
