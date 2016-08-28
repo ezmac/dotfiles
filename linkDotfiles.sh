f4ce65c0d123115d7168e3ff9b11b5c9e6ae6d1e
Tad <system.root@gmail.com>
2016-08-27 23:19:17 -0400

    add linkDotfiles.sh

---
+++ linkDotfiles.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ln -s `pwd`/.vimrc ~/.vimrc
+ln -s `pwd`/.zshrc ~/.zshrc
+ln -s `pwd`/ezmac.zsh-theme ~/.oh-my-zsh/themes/
+ln -s `pwd`/.tmux.conf ~/.tmux.conf
+ln -s `pwd`/.inputrc ~/.inputrc
+ln -s `pwd`/prompt_randomizer.js ~/.prompt_randomizer.js
+
+
