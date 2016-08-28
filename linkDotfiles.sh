ae5e1bc21ac925b9d0f3a6562a5b6922a9163166
Tad <system.root@gmail.com>
2016-08-27 23:43:55 -0400

    force symlinks for use in automation

--- linkDotfiles.sh
+++ linkDotfiles.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-ln -s `pwd`/.vimrc ~/.vimrc
-ln -s `pwd`/.zshrc ~/.zshrc
-ln -s `pwd`/ezmac.zsh-theme ~/.oh-my-zsh/themes/
-ln -s `pwd`/.tmux.conf ~/.tmux.conf
-ln -s `pwd`/.inputrc ~/.inputrc
-ln -s `pwd`/prompt_randomizer.js ~/.prompt_randomizer.js
+ln -s -f `pwd`/.vimrc ~/.vimrc
+ln -s -f `pwd`/.zshrc ~/.zshrc
+ln -s -f `pwd`/ezmac.zsh-theme ~/.oh-my-zsh/themes/
+ln -s -f `pwd`/.tmux.conf ~/.tmux.conf
+ln -s -f `pwd`/.inputrc ~/.inputrc
+ln -s -f `pwd`/prompt_randomizer.js ~/.prompt_randomizer.js
 
 
