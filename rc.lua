c0910adf5e38d45ce81a148710a029a73aff5de7
Tad Merchant <wm284@cornell.edu>
2018-01-20 11:32:16 -0500

    remove auto start programs

--- rc.lua
+++ rc.lua
@@ -33,10 +33,10 @@ function run_once(cmd)
   end
   awful.util.spawn_with_shell("pgrep -u $USER -x " .. findme .. " > /dev/null || (" .. cmd .. ")")
  end 
-awful.util.spawn_with_shell("randr --auto --output VGA1 --left-of HDMI1") 
-awful.util.spawn_with_shell("xinput set-button-map 10 1 2 1 4 5 6 7 8 3")
-run_once("syndaemon -i 1.5 -d -K")
-run_once("unclutter -idle 10")
+ --awful.util.spawn_with_shell("randr --auto --output VGA1 --left-of HDMI1") 
+ -- awful.util.spawn_with_shell("xinput set-button-map 10 1 2 1 4 5 6 7 8 3")
+ --run_once("syndaemon -i 1.5 -d -K")
+ --run_once("unclutter -idle 10")
 
 -- }}}
 
