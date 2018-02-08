b8c42dd9aec19913432a7ab9e742753db7646472
Tad M <system.root@gmail.com>
2018-02-07 22:57:49 -0500

    support for running in porta shell..

--- installNodenv.sh
+++ installNodenv.sh
@@ -2,4 +2,6 @@
 
 git clone https://github.com/nodenv/nodenv.git ~/.nodenv
 cd ~/.nodenv && src/configure && make -C src
+export PATH="$HOME/.nodenv/bin:$PATH"
+eval $(nodenv init)
 git clone https://github.com/nodenv/node-build.git $(nodenv root)/plugins/node-build
