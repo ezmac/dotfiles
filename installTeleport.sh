e2854d7d0e5afc029eea1c0ad7aebc74dcdeb1f5
tad.merchant <system.root@gmail.com>
2023-09-14 16:54:50 -0400

    Add installer for teleport; add platform to osdetection

---
+++ installTeleport.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -euo pipefail
+
+
+TELEPORT_VERSION=13.3.9
+source installerCommon.sh
+if [[ $PLATFORM == "osx" ]]; then 
+
+  brew install teleport
+else
+  prepWorkingDir "teleport"
+  # prepInstallDir "teleport"
+  TELEPORT_FILENAME="teleport-v$TELEPORT_VERSION-linux-$ARCH-bin.tar.gz"
+  URL="https://get.gravitational.com/${TELEPORT_FILENAME}"
+  if [[ ! -f $TELEPORT_FILENAME ]]; then 
+    curl -L $URL -o ${TELEPORT_FILENAME}
+  fi
+  TMP_CHECKSUM="${TELEPORT_FILENAME}.sha256"
+  curl "${URL}.sha256" > "$TMP_CHECKSUM"
+  sha256sum -c "$TMP_CHECKSUM"
+  tar xf ${TELEPORT_FILENAME}
+  mkdir -p $local_base/teleport/$TELEPORT_VERSION/
+  for cmd in "tsh" "teleport" ; do
+    mv teleport/$cmd $local_base/teleport/$TELEPORT_VERSION/
+    ln -sf $local_base/teleport/$TELEPORT_VERSION/tsh $install_base/
+  done
+fi
--- osDetection.sh
+++ osDetection.sh
@@ -10,6 +10,28 @@ elif [[ "$unamestr" == 'Darwin' ]]; then
    PLATFORM='osx'
 fi
 
+# detect architecture
+ARCH=""
+case $(uname -m) in
+   x86_64)
+      ARCH="amd64"
+      ;;
+   i386)
+      ARCH="386"
+      ;;
+   armv7l)
+      ARCH="arm"
+      ;;
+   aarch64)
+      ARCH="arm64"
+      ;;
+   **)
+      echo "ERROR: Your system's architecture isn't officially supported or couldn't be determined."
+      echo "Please refer to the installation guide for more information:"
+      echo "https://goteleport.com/docs/installation/"
+      exit 1
+      ;;
+esac
 
 
 
